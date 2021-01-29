ee42f7534a2f372779d3b3719fb9a6dc3492ca75
JahnaviSatishShanbhag <71307702+JahnaviSatishShanbhag@users.noreply.github.com>
2021-01-29 11:21:14 +0530

    Almost final thing

--- Snake and ladder/SnakeLadderBoard.java
+++ Snake and ladder/SnakeLadderBoard.java
@@ -3,6 +3,25 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.Scanner;
 import java.util.Random;
+class SnakeDialog extends Dialog implements ActionListener
+{
+    SnakeLadderBoard s;
+    SnakeDialog(Frame parent,String title)
+    {
+        super(parent,title,false);
+        s=(SnakeLadderBoard)parent;
+        setLayout(new FlowLayout());
+        setSize(380,100);
+        add(new Label(s.msg));
+        Button btn=new Button("Close");
+        add(btn);
+        btn.addActionListener(this);
+    }
+    public void actionPerformed(ActionEvent ae)
+    {
+        dispose();
+    }
+}
 public class SnakeLadderBoard extends Frame implements ActionListener
 {
     ImageIcon b;
@@ -22,6 +41,7 @@ public class SnakeLadderBoard extends Frame implements ActionListener
     int userNumber=0;
     int computerNumber=0;
     Button myButton;
+    String msg="";
     SnakeLadderBoard()
     {
         b=new ImageIcon("C:\\Users\\HP-PC\\Pictures\\Snake and ladder\\board.jpg");
@@ -40,7 +60,80 @@ public class SnakeLadderBoard extends Frame implements ActionListener
     }
     void update_position(int number,int xwidth,Graphics g)
     {
-        if (number>=1 && number<=10)
+        if (number==3)
+        {
+            xco=55*+xwidth;
+            yco=432;
+            g.fillArc(xco,yco,10,10,0,360);
+        }
+        else if (number==10)
+        {
+            xco=(55*8)+xwidth;
+            yco=544;
+            g.fillArc(xco,yco,10,10,0,360);
+        }
+        else if (number==27)
+        {
+            xco=(55*7)+xwidth;
+            yco=320;
+            g.fillArc(xco,yco,10,10,0,360);
+        }
+        else if (number==56)
+        {
+            xco=(55*3)+xwidth;
+            yco=152;
+            g.fillArc(xco,yco,10,10,0,360);
+        }
+        else if (number==72)
+        {
+            xco=(55*9)+xwidth;
+            yco=152;
+            g.fillArc(xco,yco,10,10,0,360);
+        }
+        else if (number==61)
+        {
+            xco=55+xwidth;
+            yco=96;
+            g.fillArc(xco,yco,10,10,0,360);
+        }
+        else if (number==31)
+        {
+            num=number-1;
+            xco=(55*3)+xwidth;
+            yco=600;
+            g.fillArc(xco,yco,10,10,0,360);
+        }
+        else if (number==16)
+        {
+            xco=(55*7)+xwidth;
+            yco=544;
+            g.fillArc(xco,yco,10,10,0,360);
+        }
+        else if (number==47)
+        {
+            xco=(55*4)+xwidth;
+            yco=488;
+            g.fillArc(xco,yco,10,10,0,360);
+        }
+        else if (number==66)
+        {
+            xco=(55*8)+xwidth;
+            yco=320;
+            g.fillArc(xco,yco,10,10,0,360);
+        }
+        else if (number==63)
+        {
+            xco=(55*9)+xwidth;
+            yco=376;
+            g.fillArc(xco,yco,10,10,0,360);
+        }
+        else if (number==97)
+        {
+            xco=(55*5)+xwidth;
+            yco=208;
+            g.fillArc(xco,yco,10,10,0,360);
+        }
+        else if (number>=1 && number<=10)
         {
             num=number-1;
             xco=(55*num)+xwidth;
@@ -158,10 +251,75 @@ public class SnakeLadderBoard extends Frame implements ActionListener
         }
         else if (c%4==0)
         {
-
-            g.setColor(Color.YELLOW);
+            if (initial_number%2==1)
+            {
+                g.setColor(Color.YELLOW);
+            }
+            else
+            {
+                g.setColor(Color.WHITE);
+            }
             g.fillArc(xco,yco,10,10,0,360);
             number=initial_number+number;
+            if (number>100)
+            {
+                number=initial_number;
+            }
+            else if (number==100)
+            {
+                msg="You lost the match";
+                SnakeDialog snake=new SnakeDialog(this,"Winner message box");
+                snake.setVisible(true);
+                return;
+            }
+            else if (number==3)
+            {
+                number=39;
+            }
+            else if (number==10)
+            {
+                number=12;
+            }
+            else if (number==27)
+            {
+                number=53;
+            }
+            else if (number==56)
+            {
+                number=84;
+            }
+            else if (number==72)
+            {
+                number=90;
+            }
+            else if (number==61)
+            {
+                number=99;
+            }
+            else if (number==31)
+            {
+                number=4;
+            }
+            else if (number==16)
+            {
+                number=13;
+            }
+            else if (number==47)
+            {
+                number=25;
+            }
+            else if (number==66)
+            {
+                number=52;
+            }
+            else if (number==63)
+            {
+                number=50;
+            }
+            else if (number==97)
+            {
+                number=75;
+            }
             initial_number=number;
             g.setColor(Color.MAGENTA);
             update_position(number,40,g);
@@ -175,9 +333,75 @@ public class SnakeLadderBoard extends Frame implements ActionListener
         }
         else
         {
-            g.setColor(Color.YELLOW);
+            if (initial_number1%2==1)
+            {
+                g.setColor(Color.YELLOW);
+            }
+            else
+            {
+                g.setColor(Color.WHITE);
+            }
             g.fillArc(xco1,yco1,10,10,0,360);
             number1=initial_number1+number1;
+            if (number1>100)
+            {
+                number1=initial_number1;
+            }
+            else if (number1==100)
+            {
+                msg="You won the match";
+                SnakeDialog snake=new SnakeDialog(this,"Winner message box");
+                snake.setVisible(true);
+                return;
+            }
+            else if (number1==3)
+            {
+                number1=39;
+            }
+            else if (number1==10)
+            {
+                number1=12;
+            }
+            else if (number1==27)
+            {
+                number1=53;
+            }
+            else if (number1==56)
+            {
+                number1=84;
+            }
+            else if (number1==72)
+            {
+                number1=90;
+            }
+            else if (number1==61)
+            {
+                number1=99;
+            }
+            else if (number1==31)
+            {
+                number1=4;
+            }
+            else if (number1==16)
+            {
+                number1=13;
+            }
+            else if (number1==47)
+            {
+                number1=25;
+            }
+            else if (number1==66)
+            {
+                number1=52;
+            }
+            else if (number1==63)
+            {
+                number1=50;
+            }
+            else if (number1==97)
+            {
+                number1=75;
+            }
             initial_number1=number1;
             g.setColor(Color.DARK_GRAY);
             update_position(number1,27,g);
@@ -197,4 +421,3 @@ public class SnakeLadderBoard extends Frame implements ActionListener
         appwin.setTitle("Snake and ladder");
         appwin.setVisible(true);
     }
-}
